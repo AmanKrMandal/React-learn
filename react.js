457a9b6689f482537f5443b39b711f30033b492e
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-08-27 10:44:26 +0530

    Update react.js

--- react.js
+++ react.js
@@ -1,4 +1,4 @@
-/========= CLICK EVENTS ========================
+/========= CLICK EVENTS =======================================================
 
 import logo from './logo.svg';
 import './App.css';
@@ -16,5 +16,167 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
+
+
+/============== State with functional component =================================
+
+import {useState} from 'react'
+import './App.css';
+
+function App() {
+  const [data,setData]=useState(0)
+  function updateData()
+  {
+    setData(data+1)
+   
+  }
+  console.warn("______");
+  return (
+    <div className="App">
+     <h1>{data}</h1>
+     <button onClick={updateData}>Update Data</button>
+    </div>
+  );
+}
+export default App;
+
+
+/=================== import React,{Component} from 'react' =================================================
+
+import React,{Component} from 'react'
+import './App.css';
+class  App extends Component {
+
+  constructor()
+  {
+    super();
+    this.state={
+      data:1
+    }
+  }
+  apple()
+  {
+    this.setState({data:this.state.data+1})
+  }
+  render()
+  {
+    return (
+      <div className="App">
+       <h1>{this.state.data}</h1>
+       <button onClick={()=>this.apple()}>Update Data</button>
+      </div>
+    );
+  }
+ 
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
