c78e4e698613892f27931fc35141e42963b90891
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-08-30 12:52:26 +0530

    Create 10 componentDidUpdate .js

---
+++ 10 componentDidUpdate .js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react'
+class App extends React.Component {
+  constructor()
+  {
+    super();
+    console.warn("constructor")
+    this.state={name:"anil"}
+  }
+  componentDidUpdate()
+  {
+    console.warn("componentDidUpdate")
+  }
+  render()
+  {
+    console.warn("render")
+
+    return (
+      <div className="App">
+        <h1>Component Did Mount {this.state.name}</h1>
+        <button onClick={()=>this.setState({name:"Sidhu"})}>Update</button>
+      </div>
+    );
+  }
+}
+export default App;
