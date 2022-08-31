8658faf26cb503af11ddb06e7d370567e4bff55b
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2022-08-31 11:26:52 +0530

    Update async await API .js

--- async await API .js
+++ async await API .js
@@ -1,13 +1,19 @@
  const [users, setUsers] = useState([]);
 
-    const getUsers = async () => {
-        try {
-            const response = await fetch('https://api.github.com/users');
-            setUsers(await response.json());
-        } catch (error) {
-            console.log("my error is "+ error);
-        }
+    const test = async () => {
+    try {
+        const response = await fetch("sample.json")
+        const result = await response.json();
+        setUsers(result);
+    } catch (error) {
+        console.log("my error is "+ error)
     }
+}
+test().then((result) => {
+    console.log(result)
+});
+
+
     
      const addExpenseHandler = async (expense) => {
         let response = await fetch('http://localhost:5000/add', {
