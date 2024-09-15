5e4ded91cddecd142b28666e0749e5537be5fcd4
Aman Kumar Mandal <100781280+AmanKrMandal@users.noreply.github.com>
2024-09-15 18:29:51 +0530

    Update useRef .js

--- useRef .js
+++ useRef .js
@@ -1,20 +1,27 @@
-import './App.css';
-import React,{useRef} from 'react'
+import "./App.css";
+import React, { useRef } from "react";
+
 function App() {
-  let inputRef=useRef(null);
-  function controlInput()
-  {
-  //  inputRef.current.value="abc"
-  // inputRef.current.style.display="none"
-  inputRef.current.focus()
+  let inputRef = useRef(null);
+
+  function controlInput() {
+    if (inputRef.current) {
+      if (inputRef.current.style.backgroundColor === "yellow") {
+        inputRef.current.style.backgroundColor = "white";
+      } else {
+        inputRef.current.style.backgroundColor = "yellow";
+      }
+      inputRef.current.value = "Type something here...";
+      inputRef.current.focus();
+    }
   }
+
   return (
     <div className="App">
-      <h1>useRef in React </h1>
       <input type="text" ref={inputRef} />
       <button onClick={controlInput}>Handle Input</button>
     </div>
   );
-
 }
+
 export default App;
